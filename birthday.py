35e035f07e7484d9a4f1d501192355453a163569
anoushkaalavilli <anoushkaalavilli@hanovernorwichschools.org>
2015-09-23 13:49:39 -0400

    Updated from Brython Server: 9/23/2015 1:49:39 PM

--- birthday.py
+++ birthday.py
@@ -31,6 +31,12 @@ Example Session
   And the day? 11
   Eric, you are a fall baby of the stone age.
 """
+from datetime import datetime
+from calendar import month_name
+todaymonth = datetime.today().month
+todaydate = datetime.today().day
+tmonth= str(todaymonth)
+tday= str(todaydate)
 name= input("Hello, what is your name?")
 month= input("Hi {0}, what was the name of the month you were born in?".format(name))
 year= input("And what year were you born in, {0}?".format(name))
@@ -45,10 +51,18 @@ elif month is "September" or "september" or "October" or "october" or "November"
     season="fall"
 else:
     print ("I don't quite understand what you said")
-if year is "1980" or "1981" or "1982" or "1983" or "1984" or "1985" or "1986" or "1987" or "1988" or "1989":
+if todaymonth==month and todaydate==day:
+    print ("Happy birthday!")
+elif month=="October" or "october" and day=="31":
+    print ("You were born on Halloween!")
+elif year is "1980" or "1981" or "1982" or "1983" or "1984" or "1985" or "1986" or "1987" or "1988" or "1989":
     timeperiod="eighties"
-elif int(year) is i in range (1990, 1999):
+elif year is "1990" or "1991" or "1992" or "1993" or "1994" or "1995" or "1996" or "1997" or "1998" or "1999":
     timperiod="nineties"
+elif year is "2000" or "2001" or "2002" or "2003" or "2004" or "2005" or "2006" or "2007" or "2008" or "2009" or "2010" or "2011" or "2012" or "2013" or "2014" or "2015":
+    timperiod="two thousands"
+else:
+    timeperiod="stone age"
 
 
 
